3a09875b3a1f75f3b9a11da31fc2a497d100b475
Nathan Krasney <natankrasney@gmail.com>
2022-11-08 09:45:00 +0200

    get childs

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const puppeteer = require('puppeteer');
-const {parse} = require('node-html-parser')
-const url = 'http://example.com/';
+const puppeteer = require("puppeteer");
+const { parse } = require("node-html-parser");
+const url = "http://example.com/";
 
-async function run(){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    const data = await page.$eval("html", elem => elem.innerHTML);
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  const data = await page.$eval("html", (elem) => elem.innerHTML);
 
-    const dom = parse(data);
-    const bodyElem = dom.querySelector('body');
-    console.log(bodyElem.innerHTML);
+  const dom = parse(data);
+  const bodyElem = dom.querySelector("body");
+  console.log(bodyElem.innerHTML);
 
-    const pElems = bodyElem.querySelectorAll('p');
-    console.log(pElems.length);
+  const pElems = bodyElem.querySelectorAll("p");
+  console.log(pElems.length);
 
-    await browser.close();
+  // --- get child nodes
+  console.log("get child nodes in body > div");
+  const parentEl = dom.querySelector("body > div");
+  const childElements = Object.values(parentEl.childNodes);
+  for (const childEl of childElements) {
+    console.log(childEl.innerText);
+  }
+
+  await browser.close();
 }
 
-run()
+run();
